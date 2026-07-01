8a6adb5475fce54091771c84eaf29defb6282184
Enrique Sanchez <enrique.sanchez@futurite.com>
2026-07-01 11:12:28 -0600

    feat: initial landing page implementation

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  output: 'static',
+  adapter: vercel()
+});
